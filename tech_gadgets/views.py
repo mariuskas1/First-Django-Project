4f4ce26e5e1b4f8197d9a1ab0bebdbc272aaa675
mariuskas1 <marius.kasparek@gmail.com>
2024-12-06 13:21:59 +0100

    first post view

--- tech_gadgets/views.py
+++ tech_gadgets/views.py
@@ -1,12 +1,41 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import redirect
+from django.http import HttpResponse, JsonResponse, Http404
+
 from .dummy_data import gadgets
 import json
+from django.utils.text import slugify
+from django.urls import reverse
 
 # Create your views here.
 
 def start_page_view(request):
     return HttpResponse("Das hat funktioniert")
 
-def single_gadget_view(request):
-    return HttpResponse(json.dumps(gadgets[0]), content_type="application/json" )
+def single_gadget_view(request, gadget_id):
+    if len(gadgets) > gadget_id:
+        new_slug = slugify(gadgets[gadget_id]["name"])
+        new_url = reverse("gadget_slug_url", args=[new_slug])
+        return redirect(new_url)
+    return HttpResponse("not found")
+
+
+def single_gadget_slug_view(request, gadget_slug):
+    gadget_match = {"result": "nothing"}
+
+    for gadget in gadgets:
+        if slugify(gadget["name"])==gadget_slug:
+            gadget_match = gadget
+
+    if gadget_match:
+        return JsonResponse(gadget_match)
+    raise Http404
+
+def single_gadget_post_view(request):
+    if request.method == "POST":
+        try: 
+            data = json.loads(request.body)
+            print(f"rescieved data: {data}")
+            return JsonResponse({"response": "did work"})
+        except:
+            return JsonResponse({"response": "did not work"})
+    
